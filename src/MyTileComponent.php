12d2715c139905cb1a68f5d54029c89aab9545ac
Adam Hosker <adam@hosker.info>
2020-09-13 17:43:42 +0100

    $position missing from template

--- src/MyTileComponent.php
+++ src/MyTileComponent.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class MyTileComponent extends Component
 {
+    
+   public $position;
+
+
+   public function mount(string $position)
+   {
+       $this->position = $position;
+   }
+    
+    
     public function render()
     {
         return view('dashboard-skeleton-tile::tile', [
