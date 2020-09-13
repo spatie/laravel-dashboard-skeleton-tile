bceaf90aa61b3fbc8aeefe317005e0e8cbb6180a
freekmurze <freekmurze@users.noreply.github.com>
2020-09-13 20:53:13 +0000

    Fix styling

--- src/MyTileComponent.php
+++ src/MyTileComponent.php
@@ -6,14 +6,13 @@ use Livewire\Component;
 
 class MyTileComponent extends Component
 {
-    
-   public $position;
+    public $position;
 
 
-   public function mount(string $position)
-   {
-       $this->position = $position;
-   }
+    public function mount(string $position)
+    {
+        $this->position = $position;
+    }
     
     
     public function render()
